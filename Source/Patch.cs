fe0441abcef130a9b87b0144b82c8620eac669f4
kathanon <cower@cower.se>
2023-12-09 08:27:40 +0100

    Add project files.

---
+++ Source/Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace IdeoApparelColors;
+[HarmonyPatch]
+public static class Patch {
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Ideo), nameof(Ideo.ApparelColor), MethodType.Getter)]
+    public static void ApparelColor(Ideo __instance, ref Color __result) 
+        => __result = __instance.Color;
+}
---
+++ Source/Strings.cs
@@ -0,0 +1,6 @@
+﻿namespace IdeoApparelColors;
+
+public static class Strings {
+    public const string ID = "kathanon.IdeoApparelColors";
+    public const string Name = "Ideo Apparel Colors";
+}
